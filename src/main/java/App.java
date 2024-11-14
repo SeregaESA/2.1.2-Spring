8f7cfce02419a578170af7f73b4b6d67a95b0b57
SeregaESA <esa_scb@mail.ru>
2024-11-14 11:38:52 +0300

    Hello world

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+
+        System.out.println(bean == bean2);
+        System.out.println(cat1 == cat2);
     }
 }
